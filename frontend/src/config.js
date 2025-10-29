61af2ec016d10c77c23bfca51887acc8813e7b92
Emmanuel <emmanuel.devweb@gmail.com>
2025-10-29 03:30:50 +0100

    script deploiement frontend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001/api";
-export default API_URL;
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001/api";
+export default API_URL;
