816dac01754241f36c6c441c969b2551a1769146
emmanuel <emmanuel.devweb@gmail.com>
2025-11-04 09:41:39 +0100

    modif du front package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
   server: {
     port: 5173, // Préfère le port 5173
     strictPort: false, // Trouve un autre port si 5173 est occupé
